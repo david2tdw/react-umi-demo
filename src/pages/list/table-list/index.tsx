79ebc4b03e904a6ac4ea31f4fd258a5113905c7d
Da Wei <dawei.tang@roadshare.com>
2021-01-15 16:12:48 +0800

    pro-table

--- src/pages/list/table-list/index.tsx
+++ src/pages/list/table-list/index.tsx
@@ -8,55 +8,72 @@ import ProTable, { ProColumns, ActionType } from '@ant-design/pro-table'
 
 import ProDescriptions from '@ant-design/pro-descriptions'
 
-
+import UpdateForm, { FormValueType } from './components/UpdateForm'
 import { TableListItem } from './data.d'
 import { queryRule, updateRule, addRule, removeRule } from './service'
+import CreateForm from './components/CreateForm'
+import stepForm from '@/pages/form/step-form'
 
 
 const handleAdd = async (fields: TableListItem) => {
-  const hide = message.loading('正在添加');
+  const hide = message.loading('正在添加')
   try {
-    await addRule({...fields})
-    hide();
+    await addRule({ ...fields })
+    hide()
     message.success('添加成功')
-    return true;
-  }catch (error) {
-    hide();
+    return true
+  } catch (error) {
+    hide()
     message.error('添加失败请重试！')
-    return false;
+    return false
   }
 }
 
-const handleUpdate = async (fields) => {
+const handleUpdate = async (fields: FormValueType) => {
+  const hide = message.loading('正在配置')
+  try {
+    await updateRule({
+      name: fields.name,
+      desc: fields.desc,
+      key: fields.key,
+    })
+    hide()
+    message.success('配置成功')
+    return true
 
+  } catch (error) {
+    hide()
+    message.error('配置失败请重试！')
+    return false
+  }
 }
 
 const handleRemove = async (selectedRows: TableListItem[]) => {
-  const hide = message.loading('正在删除');
+  const hide = message.loading('正在删除')
   if (!selectedRows) {
-    return true;
+    return true
   }
   try {
     await removeRule({
       key: selectedRows.map(row => row.key),
     })
-    hide();
+    hide()
     message.success('删除成功，即将刷新')
-    return true;
+    return true
   } catch (error) {
-    hide();
-    message.error('删除失败，请重试');
-    return false;
+    hide()
+    message.error('删除失败，请重试')
+    return false
   }
 }
 
 const TableList: React.FC<{}> = () => {
   const [createModalVisible, handleModalVisible] = useState<boolean>(false)
   const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false)
-  const  [stepFormValues, setStepFormValues] = useState({});
-  const actionRef = useRef<ActionType>();
-  const [row, setRow] = useState<TableListItem>();
-  const [selectedRowsState, setSelectedRows] = useState<TableListItem[]>([]);
+  const [stepFormValues, setStepFormValues] = useState({})
+  const actionRef = useRef<ActionType>()
+  const [row, setRow] = useState<TableListItem>()
+  const [selectedRowsState, setSelectedRows] = useState<TableListItem[]>([])
 
   const columns: ProColumns<TableListItem>[] = [
     {
@@ -105,15 +122,15 @@ const TableList: React.FC<{}> = () => {
       sorter: true,
       valueType: 'dateTime',
       hideInForm: true,
-      renderFormItem: (item, {defaultRender, ...rest}, form) => {
-        const status = form.getFieldValue('status');
+      renderFormItem: (item, { defaultRender, ...rest }, form) => {
+        const status = form.getFieldValue('status')
         if (`${status}` === '0') {
-          return false;
+          return false
         }
         if (`${status}` === '3') {
           return <Input {...rest} placeholder="请输入异常原因！" />
         }
-        return defaultRender(item);
+        return defaultRender(item)
       }
     },
     {
@@ -124,11 +141,11 @@ const TableList: React.FC<{}> = () => {
         <>
           <a
             onClick={() => {
-              handleUpdateModalVisible(true);
-              setStepFormValues(record);
+              handleUpdateModalVisible(true)
+              setStepFormValues(record)
             }}
           >
-          配置
+            配置
           </a>
           <Divider type="vertical" />
           <a href="">订阅警报</a>
@@ -151,7 +168,7 @@ const TableList: React.FC<{}> = () => {
             <PlusOutlined /> 新建
           </Button>
         ]}
-        request={(params, sorter, filter) => queryRule({...params, sorter, filter})}
+        request={(params, sorter, filter) => queryRule({ ...params, sorter, filter })}
         columns={columns}
         rowSelection={{
           onChange: (_, selectedRows) => setSelectedRows(selectedRows)
@@ -162,9 +179,9 @@ const TableList: React.FC<{}> = () => {
         <FooterToolbar
           extra={
             <div>
-              已选择<a style={{ fontWeight: 600}}>{selectedRowsState.length}</a>项&nbsp;&nbsp;
+              已选择<a style={{ fontWeight: 600 }}>{selectedRowsState.length}</a>项&nbsp;&nbsp;
               <span>
-              服务调用次数总计 {selectedRowsState.reduce((pre, item) => pre + item.callNo, 0)} 万
+                服务调用次数总计 {selectedRowsState.reduce((pre, item) => pre + item.callNo, 0)} 万
               </span>
             </div>
           }
@@ -173,15 +190,55 @@ const TableList: React.FC<{}> = () => {
             onClick={
               async () => {
                 await handleRemove(selectedRowsState)
-                actionRef.current?.reloadAndRest?.();
+                actionRef.current?.reloadAndRest?.()
               }
             }
           >
-          批量删除
+            批量删除
           </Button>
           <Button type="primary">批量审批</Button>
         </FooterToolbar>
       )}
+      <CreateForm onCancel={() => handleModalVisible(false)} modalVisible={createModalVisible}>
+        <ProTable<TableListItem, TableListItem>
+          onSubmit={
+            async (value) => {
+              const success = await handleAdd(value)
+              if (success) {
+                handleModalVisible(false)
+                if (actionRef.current) {
+                  actionRef.current.reload()
+                }
+              }
+            }
+          }
+          rowKey="key"
+          type="form"
+          columns={columns}
+        >
+
+        </ProTable>
+      </CreateForm>
+      {stepFormValues && Object.keys(stepFormValues).length ? (
+        <UpdateForm
+          onSubmit={async (value) => {
+            const success = await handleUpdate(value)
+            if (success) {
+              handleUpdateModalVisible(false)
+              setStepFormValues({})
+              if (actionRef.current) {
+                actionRef.current.reload()
+              }
+            }
+          }}
+          onCancel={() => {
+            handleUpdateModalVisible(false)
+            setStepFormValues({})
+          }}
+          updateModalVisible={updateModalVisible}
+          values={stepFormValues}
+        ></UpdateForm>
+      ) : null}
     </PageContainer>
   )
 }
